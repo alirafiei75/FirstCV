4d0c4f542b29ecbd7f19ec03835374dad09606c8
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-06-25 18:42:54 +0430

    set up dynamic urls and base html page

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
-    path('', index_view),
+    path('', index_view, name = 'index'),
+    path('about', about_view, name = 'about'),
+    path('education', education_view, name = 'education'),
+    path('skills', skills_view, name = 'skills'),
+    path('projects', projects_view, name = 'projects'),
+    path('contact', contact_view, name = 'contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'website/index.html')
+
+def about_view(request):
+    return render(request, 'website/about.html')
+
+def education_view(request):
+    return render(request, 'website/education.html')
+
+def skills_view(request):
+    return render(request, 'website/skills.html')
+
+def projects_view(request):
+    return render(request, 'website/projects.html')
+
+def contact_view(request):
+    return render(request, 'website/contact.html')
