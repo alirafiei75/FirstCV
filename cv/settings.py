529bb3410ef7450efb36f0432e3709e2af6db98b
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-06-28 11:47:18 +0430

    Revert "Update settings.py"
    This reverts commit 1a60d1db1e10eec3f11a84a873bf6f83f461d7b9.

--- cv/settings.py
+++ cv/settings.py
@@ -134,4 +134,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
